2b9b1dd5cb92a814e2a1524e2353750b077a91bf
Eric Liu <liueric390@gmail.com>
2023-05-31 21:27:02 -0400

    Another bug fix from discordjs update

--- index.js
+++ index.js
@@ -2,16 +2,15 @@
 const {data} = require('./main/data.js');
 
 const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({
 	intents: [
-		Intents.FLAGS.GUILDS,
-		Intents.FLAGS.GUILD_VOICE_STATES,
-		Intents.FLAGS.DIRECT_MESSAGES
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildVoiceStates,
 	],
-	partials: ["CHANNEL"]
+	partials: [Partials.channel]
 });
 
 // load commands
