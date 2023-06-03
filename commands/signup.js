9a752130e6bb5ef3774a95d4d380c0b8a3ddbf01
Eric Liu <liueric390@gmail.com>
2023-06-02 22:21:56 -0400

    Added more information with link for signup

--- commands/signup.js
+++ commands/signup.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
 
 const {VoiceChat} = require('../main/classes/commands/voice-chat.js');
 
@@ -10,9 +10,14 @@ module.exports = {
         const channel = interaction.channel;
         const user = interaction.user;
         if (!channel.isVoiceBased()) {
+			const moreInfo = new ButtonBuilder()
+				.setLabel('More Info')
+				.setStyle(ButtonStyle.Link)
+				.setURL('https://support.discord.com/hc/en-us/articles/4412085582359-Text-Channels-Text-Chat-In-Voice-Channels')
             interaction.reply({
                 content: `You must run this command in the Voice Channel (the new VC text channels) you want to sign up for`,
-                ephemeral: true
+                ephemeral: true,
+				components: [new ActionRowBuilder().addComponents(moreInfo)]
             });
             return; // stop the rest of function
         }
@@ -24,7 +29,7 @@ module.exports = {
             // if voice chat already has them
             if (voiceChat.hasUser(user.id)) {
                 interaction.reply({
-                    content: `You are already signed up for <#${channel.id}>`,
+                    content: `You are already signed up for <#${channel.id}>. Use /quit to unsignup`,
                     ephemeral: true
                 });
                 return; // stops the rest of the function
@@ -35,7 +40,7 @@ module.exports = {
             new VoiceChat(channel.id, [user.id]);
         
         interaction.reply({
-            content: `Signed up for <#${channel.id}>`,
+            content: `Signed up for <#${channel.id}>. Use /quit to unsignup`,
             ephemeral: true
         });
 	},
