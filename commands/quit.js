f29298939ea238eeb14c0fb2ab3074f79b44bd4f
Eric Liu <liueric390@gmail.com>
2023-06-03 00:22:35 -0400

    Made channel field optional

--- commands/quit.js
+++ commands/quit.js
@@ -3,22 +3,34 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('quit')
-		.setDescription('Stop being "rung" for a voice chat'),
+		.setDescription('Stop being "rung" for a voice chat')
+		.addChannelOption(option => 
+			option.setName('channel')
+				.setDescription('Select the call to stop being "rung" for, or type command in voice channel')
+				.setRequired(false)),
 	async execute(data, interaction) {
         const channel = interaction.channel;
         const user = interaction.user;
         if (!channel.isVoiceBased()) {
-			const moreInfo = new ButtonBuilder()
-				.setLabel('More Info')
-				.setStyle(ButtonStyle.Link)
-				.setURL('https://support.discord.com/hc/en-us/articles/4412085582359-Text-Channels-Text-Chat-In-Voice-Channels')
-            interaction.reply({
-                content: `You must run this command in the Voice Channel (the new VC text channels) you want to quit recieving 'rings' for`,
-                ephemeral: true,
-				components: [new ActionRowBuilder().addComponents(moreInfo)]
-            });
-            return; // stop the rest of function
-        }
+			if (!interaction.options.getChannel('channel')) {
+				const moreInfo = new ButtonBuilder()
+					.setLabel('Text Channels in Voice Channels')
+					.setStyle(ButtonStyle.Link)
+					.setURL('https://support.discord.com/hc/en-us/articles/4412085582359-Text-Channels-Text-Chat-In-Voice-Channels')
+				interaction.reply({
+					content: `Please select a channel, or run this command in the Voice Channel you want to un-sign up for`,
+					ephemeral: true,
+					components: [new ActionRowBuilder().addComponents(moreInfo)]
+				});
+			}
+			else {
+				interaction.reply({
+					content: `The channel you selected is not a Voice Channel`,
+					ephemeral: true
+				});
+			}
+			return; // stop the rest of function
+		}
         
         if (data.voiceChats.has(channel.id)) {
             const voiceChat = data.voiceChats.get(channel.id);
--- commands/signup.js
+++ commands/signup.js
@@ -5,43 +5,55 @@ const {VoiceChat} = require('../main/classes/commands/voice-chat.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('signup')
-		.setDescription('Sign up to get "rung" when someone starts a call'),
+		.setDescription('Sign up to get "rung" when someone starts a call')
+		.addChannelOption(option => 
+			option.setName('channel')
+				.setDescription('Select the call to be "rung" for, or type command in voice channel')
+				.setRequired(false)),
 	async execute(data, interaction) {
-        const channel = interaction.channel;
-        const user = interaction.user;
-        if (!channel.isVoiceBased()) {
-			const moreInfo = new ButtonBuilder()
-				.setLabel('More Info')
-				.setStyle(ButtonStyle.Link)
-				.setURL('https://support.discord.com/hc/en-us/articles/4412085582359-Text-Channels-Text-Chat-In-Voice-Channels')
-            interaction.reply({
-                content: `You must run this command in the Voice Channel (the new VC text channels) you want to sign up for`,
-                ephemeral: true,
-				components: [new ActionRowBuilder().addComponents(moreInfo)]
-            });
-            return; // stop the rest of function
-        }
+		const channel = interaction.options.getChannel('channel') || interaction.channel;
+		const user = interaction.user;
+		if (!channel.isVoiceBased()) {
+			if (!interaction.options.getChannel('channel')) {
+				const moreInfo = new ButtonBuilder()
+					.setLabel('Text Channels in Voice Channels')
+					.setStyle(ButtonStyle.Link)
+					.setURL('https://support.discord.com/hc/en-us/articles/4412085582359-Text-Channels-Text-Chat-In-Voice-Channels')
+				interaction.reply({
+					content: `Please select a channel, or run this command in the Voice Channel you want to sign up for`,
+					ephemeral: true,
+					components: [new ActionRowBuilder().addComponents(moreInfo)]
+				});
+			}
+			else {
+				interaction.reply({
+					content: `The channel you selected is not a Voice Channel`,
+					ephemeral: true
+				});
+			}
+			return; // stop the rest of function
+		}
 
-        // update or create voice chat object
-        let voiceChat = null;
-        if (data.voiceChats.has(channel.id)) {
-            voiceChat = data.voiceChats.get(channel.id);
-            // if voice chat already has them
-            if (voiceChat.hasUser(user.id)) {
-                interaction.reply({
-                    content: `You are already signed up for <#${channel.id}>. Use /quit to unsignup`,
-                    ephemeral: true
-                });
-                return; // stops the rest of the function
-            }
-            voiceChat.addUser(user.id);
-        }
-        else
-            new VoiceChat(channel.id, [user.id]);
-        
-        interaction.reply({
-            content: `Signed up for <#${channel.id}>. Use /quit to unsignup`,
-            ephemeral: true
-        });
+		// update or create voice chat object
+		let voiceChat = null;
+		if (data.voiceChats.has(channel.id)) {
+			voiceChat = data.voiceChats.get(channel.id);
+			// if voice chat already has them
+			if (voiceChat.hasUser(user.id)) {
+				interaction.reply({
+					content: `You are already signed up for <#${channel.id}>. Use /quit to unsignup`,
+					ephemeral: true
+				});
+				return; // stops the rest of the function
+			}
+			voiceChat.addUser(user.id);
+		}
+		else
+			new VoiceChat(channel.id, [user.id]);
+		
+		interaction.reply({
+			content: `Signed up for <#${channel.id}>. Use /quit to unsignup`,
+			ephemeral: true
+		});
 	},
 };
