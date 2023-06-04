ad25043d4cde52db7dcf2e54925cbb37eb351599
Eric Liu <liueric390@gmail.com>
2023-06-04 15:09:52 -0400

    Added catch statement for bug

--- main/classes/commands/voice-chat.js
+++ main/classes/commands/voice-chat.js
@@ -70,7 +70,7 @@ class VoiceChat {
         
         let channel = user.client.channels.resolve(this.channelId);
         this.userIds.forEach((value, key, map) => {
-            DiscordUser.users.get(key).ring(channel, user).catch();
+            DiscordUser.users.get(key).ring(channel, user).catch(err => {/* we don't care */});
         }); 
     }
 }
