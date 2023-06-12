251096b184ccf08fc3f58531f67911210b330a17
Eric Liu <liueric390@gmail.com>
2023-06-12 13:55:15 -0400

    Help message was wrong

--- commands/help.js
+++ commands/help.js
@@ -10,9 +10,9 @@ module.exports = {
 				new EmbedBuilder()
 				.setColor('#b574c5')
 				.setTitle('Getting Started')
-				.setDescription('RingVC is a bot which pings you automatically when someone "starts" a voice call in a channel')
+				.setDescription('RingVC is a bot which pings you automatically when someone "starts" a call (joins an empty voice channel)')
 				.addFields(
-					{ name: 'Signing up', value: 'Use /signup to start being rung when someone joins the specified voice channel' },
+					{ name: 'Signing up', value: 'Use /signup to start being rung when someone "starts" a call in the specified voice channel' },
 					{ name: 'Quitting', value: 'Use /quit or /unsignup to stop being rung for a voice channel' },
 					{ name: 'Ringing', value: 'What the bot was named after. Use /ring to ping someone once you\'re in a voice channel' },
 					{ name: 'Blocking people', value: 'Don\'t want to be rung by someone? Use /block to block people, and /unblock to unblock them. *This means you won\'t be pinged if they "start" a voice call*' },
