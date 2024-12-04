9ed01afc5591ae3b6f05ba9ac91a585a375e9ebf
Eric Liu <51763643+EricL521@users.noreply.github.com>
2024-12-03 21:35:46 -0500

    Added support server to help message

--- commands/help.js
+++ commands/help.js
@@ -18,7 +18,8 @@ module.exports = {
 					{ name: 'Blocking people', value: 'Don\'t want to be rung by someone? Use /block to block people, and /unblock to unblock them. *This means you won\'t be pinged if they "start" a voice call, however, once an unblocked person joins, you will be pinged*' },
 					{ name: 'Modes', value: 'Allows you to not ping people when joining a voice channel. Use \'/mode help\' for more info' },
 					{ name: 'Other commands', value: 'Other than the basics above, you can use /edit_filter, /get_filter, and /reset_filter, to manage your filters more in depth'},
-					{ name: 'Github', value: 'If you want to host your own version of this bot, report bugs, or just view the code, check out the github below'}
+					{ name: 'Support Server', value: 'If something\'s not working, or you just want to report bugs or suggest features, join the support server below! Or just say hello :)' },
+					{ name: 'Github', value: 'Check out the github below if you want to host your own version of this bot, or just view the code' }
 				)
 			],
 			components: [
@@ -27,6 +28,11 @@ module.exports = {
 					.setLabel('Github')
 					.setStyle(ButtonStyle.Link)
 					.setURL('https://github.com/EricL521/RingVC')
+				).addComponents(
+					new ButtonBuilder()
+					.setLabel('Support Server')
+					.setStyle(ButtonStyle.Link)
+					.setURL('https://discord.gg/bxBePEnndq')
 				)
 			],
 			ephemeral: true 
